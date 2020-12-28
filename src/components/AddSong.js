e0e773e3d64eb2596f5274ecf717db72bce6e714
HaritaJ <chiquita.prabhu@gmail.com>
2020-12-27 20:51:33 -0500

    added mock up for addSong and created action button with dialog

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,10 +1,111 @@
+import { 
+    InputAdornment, 
+    TextField, 
+    Button, 
+    Dialog, 
+    DialogContent,
+    DialogTitle, 
+    DialogActions,
+    makeStyles
+} from "@material-ui/core";
+import { Link, AddBoxOutlined } from "@material-ui/icons";
 import React from "react";
+
+const useStyles = makeStyles(theme => ({
+
+    container: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    urlInput: {
+       margin: theme.spacing(1) 
+    },
+    addSongButton: {
+        margin: theme.spacing(1) 
+     },
+     dialog: {
+         textAlign: 'center',
+     },
+     thumbnail: {
+         width: '90%'
+     }
+
+
+}))
 
 function AddSong() {
 
+    const classes = useStyles();
+    const [dialog, setDialog] = React.useState(false);
+
+    function handleCloseDialog() {
+        setDialog(false);
+    }
+
     return(
-        <div>
-            AddSong
+        <div className={classes.container}>
+            <Dialog
+                className={classes.dialog}
+                open={dialog}
+                onClose={handleCloseDialog}
+            >
+                <DialogTitle>Edit Song</DialogTitle>
+                <DialogContent>
+                    <img src="" 
+                    alt="Song thumbnail"
+                    className={classes.thumbnail}
+                    />
+                    <TextField 
+                        margin="dense"
+                        name="title"
+                        label="Title"
+                        fullWidth
+                    />
+                    <TextField 
+                        margin="dense"
+                        name="artist"
+                        label="Artist"
+                        fullWidth
+                    />
+                    <TextField 
+                        margin="dense"
+                        name="tumbnail"
+                        label="Tumbnail"
+                        fullWidth
+                    />
+                </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={handleCloseDialog} color="secondary">Cancel</Button>
+                    <Button variant="outlined" color="primary">Add Song</Button>
+                </DialogActions>
+
+            </Dialog>
+
+            <TextField 
+                className={classes.urlInput}
+                placeholder="Add Youtube or SoundCloud Url"
+                fullWidth
+                margin="normal"
+                type="url"
+                InputProps={{
+                    startAdornment: ( 
+                        <InputAdornment position="start">
+                            <Link />
+                        </InputAdornment>
+                    )
+                }}
+            />
+
+            <Button
+                className={classes.addSongButton}
+                onClick={() => setDialog(true)}
+                variant="contained"
+                color="primary"
+                endIcon={<AddBoxOutlined/>}
+            >
+                Add
+            </Button>
         </div>
     )
     
